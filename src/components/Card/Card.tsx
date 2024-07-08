f65b7a533f247b0fab9c2708d04235156fca98b6
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-07-08 12:54:55 -0500

    feat: Add meta description to index.html and update card image styling for performance and SEO

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -11,15 +11,18 @@ function Card({ id, image, width, height, color, modify, videoUrl, onDelete }: C
     return (
         <div className="flex flex-col gap-6 items-center">
             <button onClick={handleClickVideo}>
-                <img className="card-img cursor-pointer border-4"
-                    src={image}
-                    alt="Card"
-                    style={{borderColor: color,
-                        boxShadow: `0 0 1.2rem ${color}`,
-                        width: width,
-                        height: height
-                    }}
-                />
+            <img className="card-img cursor-pointer border-4"
+                src={image}
+                alt="Card"
+                style={{
+                    borderColor: color,
+                    boxShadow: `0 0 1.2rem ${color}`,
+                    width: '100%', 
+                    height: 'auto', 
+                    maxWidth: width, 
+                    maxHeight: height 
+                }}
+            />
             </button>
             <div className="flex justify-center gap-2">
                 {modify && (
