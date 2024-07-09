ec2c232b4d86082118b71533da2242b5e35288be
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-07-08 20:41:04 -0500

    feat: Add loading component to Category layout

---
+++ src/components/Utils/Loading/Loading.tsx
@@ -0,0 +1,14 @@
+export default function Loading () {
+    return (
+        <>
+            <div className="wrapper">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="shadow"></div>
+                <div className="shadow"></div>
+                <div className="shadow"></div>
+            </div>
+        </>
+    )
+}
--- src/layouts/Category/Category.tsx
+++ src/layouts/Category/Category.tsx
@@ -3,12 +3,18 @@ import TitleCategory from '../../components/TitleCategory/TitleCategory';
 import { useState, useEffect } from "react";
 import CardData from '../../models/CardData';
 import { getCards } from '../../api/api';
+import Loading from '../../components/Utils/Loading/Loading';
 
 function Category() {
     const [dataCard, setDataCard] = useState<CardData[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        getCards().then(data => setDataCard(data));
+        setLoading(true); 
+        getCards().then(data => {
+            setDataCard(data); 
+            setLoading(false); 
+        });
     }, []);
 
     const handleCardDelete = (deletedCardId: string) => {
@@ -37,35 +43,39 @@ function Category() {
 
     return (
         <div className='grid grid-cols-1 gap-10 pt-10 pb-10 md:pb-16 px-0 lg:px-14'>
-            {categories.map(category => {
-                const color = getColorByCategory(category);
-                const categoryCards = dataCard.filter(card => card.category === category);
+            {loading ? (
+                <Loading />
+            ) : (
+                categories.map(category => {
+                    const color = getColorByCategory(category);
+                    const categoryCards = dataCard.filter(card => card.category === category);
 
-                return (
-                    <div key={category}>
-                        <TitleCategory 
-                            title={category.toUpperCase()}
-                            bgColor={color}
-                        />
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 pt-10 px-5 md:px-0">
-                            {categoryCards.map(card => (
-                                <Card
-                                    id={card.id}
-                                    key={card.id}
-                                    image={card.image}
-                                    color={color}
-                                    width="380px"
-                                    height="218px"
-                                    modify={true}
-                                    videoUrl={card.video}
-                                    onDelete={handleCardDelete}
-                                    onEdit={handleCardEdit}
-                                />
-                            ))}
+                    return (
+                        <div key={category}>
+                            <TitleCategory 
+                                title={category.toUpperCase()}
+                                bgColor={color}
+                            />
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 pt-10 px-5 md:px-0">
+                                {categoryCards.map(card => (
+                                    <Card
+                                        id={card.id}
+                                        key={card.id}
+                                        image={card.image}
+                                        color={color}
+                                        width="380px"
+                                        height="218px"
+                                        modify={true}
+                                        videoUrl={card.video}
+                                        onDelete={handleCardDelete}
+                                        onEdit={handleCardEdit}
+                                    />
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })
+            )}
         </div>
     );
 }
